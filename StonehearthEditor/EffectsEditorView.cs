71a39b347ae6a28460b5189cd2ffb2123336ef57
Linda <linda@radiant-entertainment.com>
2016-03-24 11:08:45 -0700

    Add function to load up effect files in treeview on initialize

--- StonehearthEditor/EffectsEditorView.cs
+++ StonehearthEditor/EffectsEditorView.cs
@@ -19,6 +19,14 @@ namespace StonehearthEditor
 
       public void Initialize()
       {
+         effectsEditorListView.View = View.Details;
+         effectsEditorListView.GridLines = true;
+         effectsEditorListView.FullRowSelect = true;
+         effectsEditorListView.ShowItemToolTips = true;
+
+         new ModuleDataManager(MainForm.kModsDirectoryPath);
+         ModuleDataManager.GetInstance().Load();
+         ModuleDataManager.GetInstance().LoadEffectsList(effectsEditorListView);
       }
 
       private void effectsOpenFileButton_Click(object sender, EventArgs e)
