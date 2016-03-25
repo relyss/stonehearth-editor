e385dcacffa587d9fee02222a2e8140a49e0dd04
Linda <linda@radiant-entertainment.com>
2016-03-24 22:31:57 -0700

    Starting files for adding effect data (particle, lifetime, speed etc) to effect files.

---
+++ StonehearthEditor/EffectComponentsEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StonehearthEditor
+{
+   // tell you what types are valid
+   // put range limits in constructor
+   abstract class Giver { }
+
+   sealed class FloatGiver : Giver
+   {
+      private float? min;
+      private float? max;
+   }
+
+   public partial class EffectComponentsEditor : UserControl
+   {
+      // topic => parameter => param info
+      private readonly Dictionary<string, Dictionary<string, Giver>> particleOptions = new Dictionary<string, Dictionary<string, Giver>>
+      {
+         {
+            "speed", new Dictionary<string, Giver>
+         {
+            {"start", new FloatGiver() },
+            {"over_lifetime_x", new FloatGiver() }
+         } }
+      };
+
+      public EffectComponentsEditor()
+      {
+         InitializeComponent();
+      }
+   }
+}
